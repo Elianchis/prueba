2dbe06c91be0ce81899b63f7a185fba2bec2e402
elianchis <eliantamayo6@gmail.com>
2023-11-17 18:03:24 -0500

    Pruebas Yorle

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,368 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- Inicio -->
+    <header class="header">
+      <div class="logo">
+        <img src="logo1.png" alt="" >
+      </div>
+      <nav>
+        <ul class="items">
+          <li><a href="index.html">HOME</a></li>
+          <li><a href="trabaja_connosotros.html">CONTACTANOS</a></li>
+          <li><a href="login.html">Login</a></li>
+          <li><a href="registrarse.html">Registrarse</a></li>
+        </ul>
+      </nav>
+    </header>
+    <div class="contenedor1">
+      <div class="fotografia">
+        <img class="imagengra" src="./limpieza.jpg" alt="imapicture">
+      </div>
+      <div class="fotografia">
+        <h1>Hola, Bienvenidos a Limpio y Claro</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare urna in laoreet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. </p>
+        <a href="#" class="botons"><button>Saber mas</button></a>
+      </div>
+    </div>
+    <h1 style="padding: 0 30%;">Testimonio de clientes</h1>
+    <div class="trContenedor">
+      <div class="imaformacion1">
+        <img src="./persona1.jpg" alt="segundafoto">
+        <h4>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa, recusandae ab quisquam accusantium possimus error, soluta aspernatur doloribus quidem ipsa tenetur quod reprehenderit odio eligendi fugiat sequi, reiciendis dolore? Eum.</h4>
+      </div>
+      <div class="imaformacion1">
+        <img src="./persona2.jpg" alt="segundafoto">
+        <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos dolore fuga voluptates reprehenderit eveniet iure nisi cumque odio magnam, quibusdam quidem nulla, debitis culpa a consectetur consequuntur esse reiciendis doloribus!</h4>
+      </div>
+      <div class="imaformacion1">
+        <img src="./persona3.jpg" alt="segundafoto">
+        <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque debitis libero id recusandae fugiat accusantium facilis laborum iure sapiente praesentium eveniet maxime, dolor explicabo nesciunt velit architecto rerum numquam? Aperiam.</h4>
+      </div>
+    </div>
+
+    <!-- Login -->
+    <header class="header">
+      <div class="logo">
+        <img src="logo1.png" alt="" >
+      </div>
+      <nav>
+        <ul class="items">
+          <li><a href="index.html">HOME</a></li>
+          <li><a href="trabaja_connosotros.html">CONTACTANOS</a></li>
+          <li><a href="login.html">Login</a></li>
+          <li><a href="registrarse.html">Registrarse</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main class="fondo">
+      <form>
+        <h1>Iniciar sesión</h1>
+        <label for="email">Correo electrónico:</label>
+        <input type="email" id="email" name="email" required>
+        <br>
+        <label for="password">Contraseña:</label>
+        <input type="password" id="password" name="password" required>
+        <br>
+        <button type="submit">Iniciar sesión</button>
+      </form>
+    </main>
+
+    <!-- Registrarse -->
+    <header class="header">
+      <div class="logo">
+        <img src="logo1.png" alt="" >
+      </div>
+      <nav>
+        <ul class="items">
+          <li><a href="index.html">HOME</a></li>
+          <li><a href="trabaja_connosotros.html">CONTACTANOS</a></li>
+          <li><a href="login.html">Login</a></li>
+          <li><a href="registrarse.html">Registrarse</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main class="fondo">
+      <form>
+        <h1>Registrarse</h1>
+        <br>
+        <label for="nombre">Nombre:</label>
+        <input type="text" id="nombre" name="nombre" required>
+        <br>
+        <label for="mail">Direccion de Residencia:</label>
+        <input type="mail" id="mail" name="mail" required>
+        <br>
+        <label for="email">Correo electrónico:</label>
+        <input type="email" id="email" name="email" required>
+        <br>
+        <label for="Document">Numero de Documento:</label>
+        <input type="Document" id="Document" name="Document" required>
+        <br>
+        <label for="password">Contraseña:</label>
+        <input type="password" id="password" name="password" required>
+        <br>
+      </form>
+      <form>
+        <input type="image" name="image" id="Ingrese su foto de perfil" src="./fotoPersona.webp">
+        <label for="password">Contraseña:</label>
+        <input type="password" id="password" name="password" required>
+        <br>
+        <h3>Tipo de usuario</h3>
+        <select name="" id="">
+          <option value="value0">Elegir opcion</option>
+          <option value="value1">Empleado</option>
+          <option value="value2" selected>Cliente</option>
+        </select>
+        <button type="submit">Registrarse</button>
+      </form>
+    </main>
+
+    <!-- Términos y condiciones -->
+    <header class="header">
+      <div class="logo">
+        <img src="logo1.png" alt="" >
+      </div>
+      <nav>
+        <ul class="items">
+          <li><a href="index.html">HOME</a></li>
+          <li><a href="trabaja_connosotros.html">CONTACTANOS</a></li>
+          <li><a href="login.html">Login</a></li>
+          <li><a href="registrarse.html">Registrarse</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main class="fondo">
+      <div class="fondo1">
+        <h1>TERMINOS Y CONDICIONES</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare urna in laoreet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente aliquid quod ratione fuga commodi iste placeat eius provident earum a voluptatem aperiam cum dicta magni, architecto quos molestias perspiciatis dignissimos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quo earum eaque ut, neque quos enim velit qui deserunt? Suscipit neque iusto aperiam minus expedita ut. Itaque voluptatibus ipsam quod. </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui maxime obcaecati unde quisquam animi soluta maiores dolorum? Quae totam tempore non odit in vitae expedita ipsa dolore? Voluptatibus, atque nesciunt Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis ut, libero aliquid excepturi, tempore dolorem quasi officiis consequuntur modi deserunt sint quisquam dolorum aut obcaecati perferendis odio numquam molestias reprehenderit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias exercitationem dolores quasi aut quia delectus, maiores repellat iure similique consequatur, cum asperiores numquam minima nostrum enim ab in maxime. Reprehenderit.</p>
+      </div>
+    </main>
+
+    <!-- Trabaja con nosotros -->
+    <header class="header">
+      <div class="logo">
+        <img src="logo1.png" alt="" >
+      </div>
+      <nav>
+        <ul class="items">
+          <li><a href="index.html">HOME</a></li>
+          <li><a href="trabaja_connosotros.html">CONTACTANOS</a></li>
+          <li><a href="login.html">Login</a></li>
+          <li><a href="registrarse.html">Registrarse</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main class="fondo">
+      <div class="fondo1">
+        <h1>Trabaja con nosotros</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare urna in laoreet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. </p>
+        <p>Contacta con nosotros: <a href="mailto:example@example.com">example@example.com</a></p>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  // ... (puedes agregar lógica específica del componente aquí)
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+body {
+    font-family:'Times New Roman', Times, serif;
+    margin: 0;
+    padding: 0;
+}
+header{
+     display: flex;
+     flex-wrap:unset;
+     justify-content: space-between;
+     align-items: center;
+     height: 120px;
+     padding: 15px ;
+     background-color: rgb(255, 255, 255);
+     font-size: 70px;
+     width: 100%;
+ 
+ }
+ .logo{
+     display: flex;
+     align-items: center;
+     
+     
+ }
+ .logo img{
+     height: 100px;
+     margin-right: 10px;
+     
+ }
+
+nav ul {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    list-style:none;
+    margin-right: 30px;
+}
+
+nav ul li a {
+    display: block;
+    color: rgb(0, 0, 0);
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 20px;
+}
+
+nav ul li a:hover {
+    color:gold;
+}
+
+form {
+    width: 300px;
+    padding: 20px;
+    background-color: rgba(255, 255, 255, 0.68);
+    border-radius: 5px;
+    box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.3);
+    float: left;
+}
+
+form label {
+    display: block;
+    margin-top: 10px;
+}
+
+form input {
+    width: 90%;
+    padding: 10px;
+    margin-top: 5px;
+
+    margin: 5px 0 22px 0;
+    display: inline-block;
+    border-color:#333;
+    background: #f1f1f1;
+}
+
+form button {
+    display: block;
+    width: 100%;
+    padding: 15px 20px;
+    margin-top: 10px;
+    background-color: #333;
+    color: white;
+    border: none;
+    cursor: pointer;
+    margin: 8px 0;
+    opacity: 0.9;
+    
+}
+
+form button:hover {
+    background-color: #555;
+    opacity:1;
+}
+.fondo{
+    background-image:url('./FotoFondo.jpg');
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    margin: 0;
+}
+.contenedor1{
+    display: flex;
+    grid-template-columns: repeat(2, 1fr);
+    top:668px;
+    height: 575px;
+}
+.fotografia{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+   
+    padding:0 20px;
+}
+.imagengra{
+    height: 390px;
+    width: 523px;
+    margin: 0 auto;
+    display: flex;
+}
+.texto1{
+    font-family:'Times New Roman', Times, serif;
+    font-style: normal;
+    font-weight: 200;
+    font-size: 26px;
+    line-height: 29px;
+    
+}
+.botons button{
+    margin-left: 20px;
+    font-weight: 700;
+    color: darkslateblue;
+    padding: 25px 45px;
+    background: whitesmoke;
+    border-radius: 50px;
+    border-color: #555;
+    cursor: pointer;
+    transition: all 0.3s ease 0s;
+}
+.botons button:hover{
+    background-color:beige;
+    color:mediumorchid;
+    transform: scale(1.1);
+}
+
+.trContenedor{
+    flex-wrap: wrap;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    
+}
+.imaformacion1{
+    color: cadetblue 75%;
+    left: 0px;
+    top: 869px;
+    width: 400px;
+    height: 300px;
+    left: 0px;
+    top: 869px;
+    align-content: space-between;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding:0 50px;
+}
+
+
+.imaformacion1 img{
+    width: 200px;
+    height: 200px;
+    left: 220px;
+    top: 946px;
+    border-radius: 60%;
+    border: 7px solid #000000ad;
+    
+}
+.fondo1{
+    width: 700px;
+    padding: 20px;
+    background-color: rgba(255, 255, 255, 0.68);
+    border-radius: 5px;
+    box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.3);
+    float: left;
+}
+footer {
+    text-align: center;
+    padding: 3px;
+    background-color: rgba(122, 113, 111, 0.493);
+    color: white;
+    display: flex;
+    flex-wrap:unset;
+    justify-content: space-between;
+    align-items: center;
 }
 </style>
